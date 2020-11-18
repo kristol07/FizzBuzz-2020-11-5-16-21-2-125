b2227c391899b04ce5ed3bc5cf1f7cbbcccd08ab
Lin Liu <LLiu41@slb.com>
2020-11-18 15:35:39 +0800

    refactor: remove duplicate conditions

--- FizzBuzz/HelloWorld.cs
+++ FizzBuzz/HelloWorld.cs
@@ -18,32 +18,24 @@ namespace FizzBuzz
 
         public string Tranlate(int count)
         {
-            if (count % 3 == 0 && count % 5 == 0 && count % 7 == 0)
-            {
-                return "FizzBuzzWhizz";
-            }
-
-            if (count % 3 == 0 && count % 5 == 0)
-            {
-                return "FizzBuzz";
-            }
+            string word = string.Empty;
 
             if (count % 3 == 0)
             {
-                return "Fizz";
+                word += "Fizz";
             }
 
             if (count % 5 == 0)
             {
-                return "Buzz";
+                word += "Buzz";
             }
 
             if (count % 7 == 0)
             {
-                return "Whizz";
+                word += "Whizz";
             }
 
-            return count.ToString();
+            return word.Length == 0 ? count.ToString() : word;
         }
     }
 }
